3de8432ecb883416161dbfe435234d5122a8ae81
SOHAM DAS <sohamdas.nest@gmail.com>
2024-02-12 21:57:16 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");;
+const app = express();
 
 dotenv.config()
+
+PORT = process.env.PORT || 5050;
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
