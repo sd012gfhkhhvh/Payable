a49cc35f404dc262fe5a88ec65121c6ff59f922e
SOHAM DAS <sohamdas.nest@gmail.com>
2024-02-13 04:04:01 +0530

    handled undefined routes and implemented error handling middleware

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,17 @@ app.use(express.json());
 //route
 app.use("/api/v1", rootRouter)
 
+//handle undefined routes
+app.all("*", (req, res) => {
+    res.status(404).json({message: "route not found"})
+})
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).json({message: "server broke !!"})
+})
+
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
