4dcf847f70938d12a3c91d7656b3b9e442cb65b5
SOHAM DAS <sohamdas.nest@gmail.com>
2024-02-22 04:18:45 +0530

    initiated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// import route elements
+import { Signin } from './pages/signin'
+import { Signup } from './pages/signup'
+import { Dashboard } from './pages/dashboard'
+import { SendMoney } from './pages/sendMoney'
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
