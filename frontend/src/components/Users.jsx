3a07893bdf9fc2651f42a3d53fe899bff362ef17
SOHAM DAS <sohamdas.nest@gmail.com>
2024-03-03 03:19:15 +0530

    created FE

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { Button } from "./Button"
+
+import { useNavigate } from "react-router-dom";
+
+export const Users = () => {
+
+    // Replace with backend call
+    const [users, setUsers] = useState(
+        [
+            {
+                firstName: "Soham",
+                lastName: "Das",
+                _id: 1
+            },
+            {
+                firstName: "Harkirat",
+                lastName: "Singh",
+                _id: 2
+            }
+        ]
+    );
+
+    return <>
+        <div className="font-bold mt-6 text-lg">
+            Users
+        </div>
+        <div className="my-2">
+            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+        </div>
+        <div>
+            {users.map(user => <User key={user._id} user={user} />)}
+        </div>
+    </>
+}
+
+function User({ user }) {
+    const navigate = useNavigate();
+
+    return <div className="flex justify-between">
+        <div className="flex">
+            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                <div className="flex flex-col justify-center h-full text-xl">
+                    {user.firstName[0]}
+                </div>
+            </div>
+            <div className="flex flex-col justify-center h-ful">
+                <div>
+                    {user.firstName} {user.lastName}
+                </div>
+            </div>
+        </div>
+
+        <div className="flex flex-col justify-center h-ful">
+            <Button onClick={() => navigate("/send")} label={"Send Money"} />
+        </div>
+    </div>
+}
--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,6 +1,15 @@
+import { Appbar } from "../../components/Appbar"
+import { Balance } from "../../components/Balance"
+import { Users } from "../../components/Users"
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="">
+      <Appbar />
+      <div className="p-6">
+        <Balance value="5000" />
+        <Users />
+      </div>
+    </div>
   )
 }
