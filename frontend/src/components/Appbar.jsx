acf623760deed30dcb5c626347989ad0e32a76ea
SOHAM DAS <sohamdas.nest@gmail.com>
2024-03-04 05:18:42 +0530

    added logout functionality

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react"
+import { Button } from "./Button"
+import { isMe } from "../../utils/me"
+import { useNavigate } from "react-router-dom"
 /* eslint-disable react/prop-types */
-export const Appbar = ({firstName}) => {
+export const Appbar = ({ firstName }) => {
+    const navigate = useNavigate();
+
+    const [isLoggedin, setIsLoggedin] = useState(false)
+
+    useEffect(() => {
+        isMe().then((response) => {
+            if (response) {
+                setIsLoggedin(true)
+            }
+            else {
+                setIsLoggedin(false)
+            }
+        })
+    }, [])
+
+    const handleLogout = () => {
+        if (isLoggedin) {
+            localStorage.removeItem("token")
+            alert("Logged Out Successfully")
+            navigate("/signin")
+        }
+    }
+
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
             Payable
@@ -13,6 +40,9 @@ export const Appbar = ({firstName}) => {
                     {firstName[0]}
                 </div>
             </div>
+            {isLoggedin && <div className="h-full flex justify-center items-center mt-1">
+                <Button label={"Logout"} onClick={handleLogout} />
+            </div>}
         </div>
     </div>
 }
