ae5850e88dc082775a3b39a5e45ec9f01f39885b
SOHAM DAS <sohamdas.nest@gmail.com>
2024-03-05 17:01:31 +0530

    modified

--- frontend/utils/ProtectedRoute.jsx
+++ frontend/utils/ProtectedRoute.jsx
@@ -8,13 +8,17 @@ import { isMe } from './me'
 export const ProtectedRoute = ({ Component, loggedInRoute, loggedOutRoute }) => {
     const navigate = useNavigate()
 
+    //navigate according to the wheather the user is logged in or not
     useEffect(() => {
         isMe().then((response) => {
+            //Not logedin
             if (!response) {
                 if (loggedOutRoute) {
-                    navigate(loggedOutRoute, {replace: true}) // stopts the user from navigate back to the previous page and make them stay at the signin page
+                    navigate(loggedOutRoute, { replace: true }) // stopts the user from navigate back to the previous page and make them stay at the signin page
                 }
-            } else {
+            }
+            //Logged in 
+            else {
                 if (loggedInRoute) {
                     navigate(loggedInRoute)
                 }
